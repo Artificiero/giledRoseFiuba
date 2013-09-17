59272e3ab536b36816e37ea23f10436b3dbc46d7
TomR <tomasreale@gmail.com>
2013-09-17 14:14:24 -0300

    probando

---
+++ src/main/java/ar/fiuba/tecnicas/giledrose/ItemWrapper.java
@@ -0,0 +1,18 @@
+package main.java.ar.fiuba.tecnicas.giledrose;
+
+public abstract class ItemWrapper {
+	protected Item item;
+	
+	protected abstract void updateSellin();
+	protected abstract void updateQuality();
+
+	public void update(){
+		this.updateQuality();
+		this.updateSellin();
+	}
+	
+	protected void addToQuality(int quantity){
+		int newQuality = this.item.getQuality() + quantity ;
+		this.item.setQuality( newQuality );
+	}
+}
