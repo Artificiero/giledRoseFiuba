e99e9279ba55ae00e6d79561b7eb9ca7a53bb2f1
TomR <tomasreale@gmail.com>
2013-09-19 18:04:59 -0300

    limpio couts

--- src/main/java/ar/fiuba/tecnicas/giledrose/ItemFactory.java
+++ src/main/java/ar/fiuba/tecnicas/giledrose/ItemFactory.java
@@ -14,37 +14,29 @@ public class ItemFactory {
 	ItemWrapper createItemWrapper(Item item){
 		
 		String name = item.getName().toUpperCase();
-		System.out.println(name);
 		if ( name.contains( AGED ) ){
-			System.out.println("AGED");
 			return new AgedBrie(item);						
 		}
 		
 		if ( name.contains( DEXTERITY )){
-			System.out.println("dex");
 			return new Dexterity(item);
 		}
 
 		if ( name.contains( SULFURAS )){
-			System.out.println("SULF");
 			return new Sulfuras(item);
 		}
 		
 		if ( name.contains( CONJURED )){
-			System.out.println("CONJ");
 			return new Conjured(item);
 		}
 		
 		if ( name.contains( BACKSTAGE )){
-			System.out.println("BACKS");
 			return new BackStage(item);
 		}
 		
 		if ( name.contains ( ELIXIR)){
-			System.out.println("ELIX");
 			return new Elixir(item);			
 		}
-		System.out.println("FAAAAAAAAAIL");
 		return null;
 	}
 	
